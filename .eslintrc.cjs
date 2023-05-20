9028e43f36c57a6347a1e9af28772f1affe89743
longo-andrea <longoandrea.dev@gmail.com>
2023-05-20 15:26:22 +0200

    feat: add ESLint configurations

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,12 +4,11 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 module.exports = {
 	root: true,
 	extends: [
-		'plugin:vue/vue3-essential',
-		'eslint:recommended',
-		'@vue/eslint-config-typescript',
-		'@vue/eslint-config-prettier/skip-formatting',
+		'@longo-andrea/eslint-config-typescript',
+		'@longo-andrea/eslint-config-vue',
+		'@longo-andrea/eslint-config-base',
 	],
-	parserOptions: {
-		ecmaVersion: 'latest',
+	rules: {
+		'import/named': 'off',
 	},
 }
