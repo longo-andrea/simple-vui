26a5af81f787af44c77f443a9fe90d55de239227
longo-andrea <longoandrea.dev@gmail.com>
2023-05-24 22:07:36 +0200

    fix: package exports

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,2 +1,2 @@
-export * from './components/avatar/Avatar'
-export * from './components/avatarGroup/AvatarGroup'
+export { default as Avatar } from './components/avatar/Avatar'
+export { default as AvatarGroup } from './components/avatarGroup/AvatarGroup'
